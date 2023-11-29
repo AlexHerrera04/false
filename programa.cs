96bdd56839eacaf6f8076285b8785cf717b26354
AlexHerrera04 <145123004+AlexHerrera04@users.noreply.github.com>
2023-11-29 18:24:22 +0100

    Update programa.cs

--- programa.cs
+++ programa.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hola desde C#! Este es un programa simple en una terminal de Windows.");
+        Console.WriteLine("Hola desde C#!");
         Console.Write("Por favor, ingresa tu nombre: ");
         string nombre = Console.ReadLine();
         Console.WriteLine($"Hola, {nombre}! Bienvenido a C#.");
